661d6185aa015ab6fd8b16c8428090d83b988320
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-08-11 15:00:28 +0200

    the first API calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,20 @@
-// Global app controller
-import x from "./test";
+import axios from "axios";
 
-console.log(`I imported ${x} from test file`);
+//687a5859aeb66b8932974dabdd48036e
+//https://www.food2fork.com/api/search
+
+async function getResults(query) {
+  const key = "687a5859aeb66b8932974dabdd48036e";
+
+  try {
+    const res = await axios(
+      `https://www.food2fork.com/api/search?key=${key}&q=${query}`
+    );
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+
+getResults("chicken");
