6c7332ca9e20f8335eea2e1a659472ea7efd5db9
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-08-12 20:51:03 +0200

    Building the search view model

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,34 @@
 import Search from "./models/Search";
+import { elements } from "./views/base";
+import * as searhcView from "./views/searchView";
+// Global state of the app
+// - Search object
+// - current recipe object
+// - SHopping list object
+//- Liked object
 
-const search = new Search("pizza");
+const state = {};
 
-console.log(search);
+const controllSearch = async () => {
+  //1. get the query from the view
+  const query = searhcView.getInput();
+
+  if (query) {
+    // 2. new searhc object and add to state
+    state.search = new Search(query);
+
+    //3 prepare UI for results
+    searhcView.clearInput();
+    searhcView.clearResults();
+    //4 Search for recipes
+    await state.search.getResults();
+
+    //5 render result on UI
+    searhcView.renderResult(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  controllSearch();
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -13,7 +13,7 @@ export default class Search {
         `https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
       );
       this.result = res.data.recipes;
-      console.log(recipes);
+      //console.log(this.result);
     } catch (error) {
       alert(error);
     }
