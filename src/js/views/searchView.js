66a1afc6619f81f733bb85b458e7c0d15f01621b
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-08-13 20:48:58 +0200

    limit the search result to 17 words

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,6 +10,32 @@ export const clearResults = () => {
   elements.searchResList.innerHTML = "";
 };
 
+// Shorten the titles of search result
+/*
+search result: "Pasta with tomato and spinach". 
+acc: 0 / acc + curr.lenth = 5 / newTitle = ['pasta']
+acc: 5 / acc + curr.lenth = 9 / newTitle = ['pasta', 'with']
+acc: 9 / acc + curr.lenth = 15 / newTitle = ['pasta', 'with', 'tomato']
+(now it's bigger than limit=17)
+!acc: 15 / acc + curr.lenth = 18 / newTitle = ['pasta', 'with', 'tomato']
+!acc: 18 / acc + curr.lenth = 24 / newTitle = ['pasta', 'with', 'tomato']
+*/
+const limitRecipeTitle = (title, recipe, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+
+    //return the result
+    return `${newTitle.join(" ")} ...`;
+  }
+  return title;
+};
+
 const renderRecipe = recipe => {
   const markup = `
     <li>
@@ -20,7 +46,9 @@ const renderRecipe = recipe => {
   }">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitRecipeTitle(
+                              recipe.title
+                            )}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
