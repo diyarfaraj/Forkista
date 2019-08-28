ffeb2a6817a66139918a400c76d41dbd64c70d66
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-08-28 17:42:29 +0200

    build the likes model

---
+++ src/js/models/Likes.js
@@ -0,0 +1,27 @@
+export default class Likes {
+  constructor() {
+    this.likes = [];
+  }
+
+  addLike(id, titel, author, img) {
+    const like = { id, titel, author, img };
+    this.like.push(like);
+
+    return like;
+  }
+
+  deleteLike(id) {
+    const index = this.likes.findIndex(el => el.id === id);
+    //[2,4,6] splice[1, 1] --> returns 4, and original array is mutated [2,6]
+    //[2,4,6] slice[1, 1] --> returns 4, and original array is not mutated [2,4,6]
+    this.likes.splice(index, 1);
+  }
+
+  isLiked(id) {
+    return this.likes.findIndex(el => el.id === id) !== -1;
+  }
+
+  getNumLikes() {
+    return this.likes.length;
+  }
+}
